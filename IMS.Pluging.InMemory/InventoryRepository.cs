357d38d526c0669f981ed3ce31364472ce748aa0
PetrusJVollgraaff <petrusvollgraaff1988@hotmail.co.za>
2025-09-08 16:21:53 +0200

    Add project files.

---
+++ IMS.Pluging.InMemory/InventoryRepository.cs
@@ -0,0 +1,20 @@
+﻿//using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.Pluging.InMemory
+{
+    public class InventoryRepository : UseCases.PluginInterfaces.IInventoryRepository
+    {
+        private List<CoreBusiness.Inventory> _inventories;
+
+        public InventoryRepository()
+        {
+            _inventories = new List<CoreBusiness.Inventory>()
+            {
+                new CoreBusiness.Inventory { }
+            };
+        }
+
+        public Task<IEnumerable<CoreBusiness.Inventory>> GetInventoriesByNameAsync(string name) { 
+        }
+    }
+}
---
+++ IMS.UseCases/PluginInterfaces/IInventoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using IMS.CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.PluginInterfaces
+{
+    public class IInventoryRepository
+    {
+        internal async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
