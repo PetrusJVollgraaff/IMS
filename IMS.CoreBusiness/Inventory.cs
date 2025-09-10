f695d40e22d744fe6c08c02a6c81bdc2d53f28c3
PetrusJVollgraaff <petrusvollgraaff1988@hotmail.co.za>
2025-09-10 09:07:29 +0200

    updeage

---
+++ IMS.CoreBusiness/Inventory.cs
@@ -0,0 +1,10 @@
+﻿namespace IMS.CoreBusiness
+{
+    public class Inventory
+    {
+        public int InventroyId { get; set; }
+        public string InventroyName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
--- IMS.Pluging.InMemory/InventoryRepository.cs
+++ IMS.Pluging.InMemory/InventoryRepository.cs
@@ -1,20 +1,27 @@
-﻿//using IMS.UseCases.PluginInterfaces;
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
 
 namespace IMS.Pluging.InMemory
 {
-    public class InventoryRepository : UseCases.PluginInterfaces.IInventoryRepository
+    public class InventoryRepository : IInventoryRepository
     {
-        private List<CoreBusiness.Inventory> _inventories;
+        private List<Inventory> _inventories;
 
         public InventoryRepository()
         {
-            _inventories = new List<CoreBusiness.Inventory>()
+            _inventories = new List<Inventory>()
             {
-                new CoreBusiness.Inventory { }
+                new Inventory {InventroyId = 1, InventroyName = "Bike Seat", Quantity=10, Price=2 },
+                new Inventory {InventroyId = 2, InventroyName = "Bike Body", Quantity=10, Price=15 },
+                new Inventory {InventroyId = 1, InventroyName = "Bike Wheels", Quantity=20, Price=8 },
+                new Inventory {InventroyId = 1, InventroyName = "Bike Pedels", Quantity=20, Price=1 },
             };
         }
 
-        public Task<IEnumerable<CoreBusiness.Inventory>> GetInventoriesByNameAsync(string name) { 
+        public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name) {
+            if (string.IsNullOrWhiteSpace(name)) return await Task.FromResult(_inventories);
+
+            return _inventories.Where(x => x.InventroyName.Contains(name, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
